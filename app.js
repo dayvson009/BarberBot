12acc773dbdff935daa7843d0aaaed06e682b3c3
unknown <dayvson.vinicius@hotmail.com>
2022-05-15 15:22:02 -0300

    Criação dos endpoints

--- app.js
+++ app.js
@@ -8,6 +8,62 @@ const app = express()
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+/**
+ * Endpoint para salvar Conversas
+ * 
+ */
+
+// POST
+
+/**
+ * Endpoint para buscar as datas disponivéis do agendamento
+ * 
+ */
+
+// POST
+
+/**
+ * Endpoint para buscar horários disponivéis do agendamento
+ * 
+ */
+
+// POST
+
+/**
+ * Endpoint salvar o agendamento
+ * TODO Verificar antes se alguém já escolheu a hora selecionada (evitar duplicidade)
+ * 
+ */
+
+// POST
+
+/**
+ * Endpoint salvar o lembrete
+ * 
+ */
+
+// POST
+
+/**
+ * Endpoint disparar para o cabeleireiro a lista atualizada de agendamentos do dia
+ * 
+ */
+
+// POST
+
+/**
+ * Aplicação para verficiar o status das conversas
+ * 
+ */
+
+// POST
+
+/**
+ * Aplicação dispara o Lembrete, deve rodar a cada 1 minuto
+ * 
+ */
+
+// POST
 
 showProducts()
 
