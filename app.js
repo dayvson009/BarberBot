ad6bfd1e82ae9faadfbeb016c0da30e11694af1f
unknown <dayvson.vinicius@hotmail.com>
2022-05-17 23:31:07 -0300

    Criando endpoints e funcões iniciais

--- app.js
+++ app.js
@@ -12,59 +12,73 @@ app.use(bodyParser.json())
  * Endpoint para salvar Conversas
  * 
  */
-
-// POST
+app.post('/save-message', async (req, res) => {
+  
+  res.send("Pegou")
+})
 
 /**
  * Endpoint para buscar as datas disponivéis do agendamento
- * 
+ * pegar só 30 dias
  */
-
-// POST
+app.post('/get-datas', async (req, res) => {
+  
+  res.send("Pegou")
+})
 
 /**
- * Endpoint para buscar horários disponivéis do agendamento
+ * Endpoint para buscar horários da data escolhida disponivéis do agendamento
  * 
  */
-
-// POST
+app.post('/get-hours', async (req, res) => {
+  
+  res.send("Pegou")
+})
 
 /**
  * Endpoint salvar o agendamento
  * TODO Verificar antes se alguém já escolheu a hora selecionada (evitar duplicidade)
  * 
  */
-
-// POST
+app.post('/save', async (req, res) => {
+  
+  res.send("Pegou")
+})
 
 /**
  * Endpoint salvar o lembrete
  * 
  */
-
-// POST
+app.post('/add-reminder', async (req, res) => {
+  
+  res.send("Pegou")
+})
 
 /**
  * Endpoint disparar para o cabeleireiro a lista atualizada de agendamentos do dia
  * 
  */
-
-// POST
+app.post('/send-list-scheduling', async (req, res) => {
+  
+  res.send("Pegou")
+})
 
 /**
  * Aplicação para verficiar o status das conversas
  * 
  */
-
-// POST
+const updateStatusMessages = () => {
+  console.log('cuidado ao olhar pra cima')
+}
 
 /**
  * Aplicação dispara o Lembrete, deve rodar a cada 1 minuto
  * 
  */
-
-// POST
+const verifyTimeScheduling = () => {
+  console.log("Se houver lembretes agendados envia um chatbot paa o cliente")
+}
 
 showProducts()
 
-app.listen
+app.listen(port, console.log("Aplicação Rodando na porta ", port))
